d32c96a9ff06f546349e6a3ce3d08df28c81fa48
rakesh468 <rakeshkumarg2000@gmail.com>
2022-03-28 02:25:21 -0700

    make it better

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -17,10 +17,10 @@ function Register() {
     e.preventDefault();
     try {
       await axios.post("/user/register", { ...user });
-      localStorage.setItem("firstlogin", true);
-      window.location.href = "/";
+      localStorage.setItem("firstlogin", true)
+      window.location.href = "/"
     } catch (error) {
-      alert(error.response.data.msg);
+      alert(error.response.data.msg)
     }
   };
   return (
