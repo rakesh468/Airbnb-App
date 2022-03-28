f0916d48a0fabee059721f26986ed64ee1e315b4
rakesh468 <rakeshkumarg2000@gmail.com>
2022-03-28 03:18:47 -0700

    make it better

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import Button from "@mui/material/Button";
 
+// Register Routes//
 function Register() {
   const [user, setuser] = useState({
     name: "",
